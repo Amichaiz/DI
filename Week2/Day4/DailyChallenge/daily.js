6aec277484a9bc9df40a5d91f65245d97f529845
Malasa123 <adanemalasa@gmail.com>
2022-03-24 10:26:58 +0200

    daily challenge added prompt

--- Week2/Day4/DailyChallenge/daily.js
+++ Week2/Day4/DailyChallenge/daily.js
@@ -16,32 +16,36 @@
 
 
 
-let sentance = ["Hello", "World", "in", "a", "frame"];
-
+// let sentance = ["Hello", "World", "inhjbj", "a", "frjhbjhame"];
+let sentance = prompt("write a sentance ")
 starWord(sentance);
 
 function starWord(array) {
-    let bigWord = biggerWord(array);
+    let sentToArr = array.split(" "); // convert the sentence we recived from the user to an array.
+
+    let bigWord = biggerWord(sentToArr); // reciving the  length of the longet word, which determent the size of the frame.
     let sign = "*";
 
-    let frame = sign.repeat(bigWord + 4)
-    
-    console.log(`${frame}`);
-    for (let j = 0; j < array.length; j++) {
+    let frame = sign.repeat(bigWord + 4) // creating the frame.
+
+    console.log(`${frame}`); //console log the frame.
+    for (let j = 0; j < sentToArr.length; j++) {
         let empty = " ";
-        let currWord = array[j];
-        let space = bigWord - currWord.length ;
+        let currWord = sentToArr[j];                          
+        let space = bigWord - currWord.length;
         console.log(`* ${currWord} ${empty.repeat(space)}*`);
     }
-    
+
     console.log(`${frame}`);
 
 }
 
 function biggerWord(array) {
+    // let sentToArr = array.split(" "); // convert the sentence form the user to an array.
+console.log(array);
     let bigWord = 0;
     for (let i = 0; i < array.length; i++) {
-        currWord = array[i];
+        let currWord = array[i];
         let bigCurr = currWord.length;
         if (bigCurr > bigWord) {
             bigWord = bigCurr
---
+++ Week2/Day4/ExerciseXPGold/xpGold.js
@@ -0,0 +1,26 @@
+// ex1.
+
+var fvfgf = " ";
+isBlank(fvfgf);
+function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+
+    // if (!str || str == null || str == " ") {
+    //     console.log(true);
+    // } else {
+    //     console.log(false);
+    // }
+}
+// ex2.
+
+
+let named = "adan eme"
+let abbrev = named.split();
+console.log(abbrev);
+function abbrevName(name){
+
+    for( let i = 0; i < 2; i++){
+
+    }
+
+}
