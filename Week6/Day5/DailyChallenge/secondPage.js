0ce76307bb43699383e0fd231e11bc6ffee31bbe
Malasa123 <adanemalasa@gmail.com>
2022-04-29 00:15:06 +0300

    mini proj to do list

--- Week6/Day5/DailyChallenge/secondPage.js
+++ Week6/Day5/DailyChallenge/secondPage.js
@@ -1,20 +1,21 @@
+let tasks = JSON.parse(localStorage.getItem('Task')) || [];
+
 const displayTask = () => {
-    let tasks = JSON.parse(localStorage.getItem('Task'));
-console.log(tasks);
-    let strHTML = `<table>`
+
+    let strHTML = `<table><tr><th>/</th><th>TITLE</th><th>START-TASK</th><th>DAYS LEFT</th>`
     let display = document.getElementsByClassName('wraper')[0];
     for (let i = 0; i < tasks.length; i++) {
         let currObj = tasks[i]
-        strHTML += `<tr class="task task-${i}"> <td onclick="deleteTask(this,${i})">X</td>`
+        strHTML += `<tr class="task task-${i}"> <td onclick="completeTask(this,${i})" ondblclick="deleteTask(this,${i})">❌</td>`
         for (const key in currObj) {
             let element = currObj[key];
-            if (element === false) {
-                element = 'unCompleted'
-                strHTML += `<td>${element}</td>`
+            if (element === false || key === "endTime" || key === "description") {
+                continue;
+            } else if (key === "name") {
+                strHTML += `<td onclick="displayDescription(this,${i})">${element}</td>`
             } else {
                 strHTML += `<td>${element}</td>`
             }
-
         }
         strHTML += `</tr>`;
     }
@@ -22,18 +23,40 @@ console.log(tasks);
     display.innerHTML = strHTML;
 }
 
-const deleteTask = (e, i) => {
-    console.log(i);
-    e.parentElement.remove();
-    for (let j = 0; j < tasks.length; j++) {
-        localStorage.removeItem('Task')
-        if (j === i) {
-            tasks.splice(j, 1)
-            localData.splice(j, 1)
-            localStorage.setItem('Task', tasks)
-            console.log(localData);
-
-        }
-
+const completeTask = (e, n) => {
+    let color = e;
+    if (e.textContent === '❌') {
+        tasks[n]['isComplete'] = true;
+        e.textContent = '✔️';
+    } else {
+        tasks[n]['isComplete'] = false;
+        e.textContent = '❌';
     }
 }
+const displayDescription = (e, n) => {
+    if (e.textContent === tasks[n]['name']) {
+        e.textContent = `description: ${tasks[n]['description']}`;
+    } else {
+        e.textContent = tasks[n]['name'];
+    }
+}
+
+const deleteTask = (i) => {
+    let del = confirm('you sure you want to delete this task?');
+    if (del) {
+        tasks.splice(i, 1)
+        localStorage.setItem('Task', JSON.stringify(tasks));
+        displayTask();
+    }
+}
+
+//create calander
+
+// let start = new Date()
+// let end = ((new Date()) + (86400000 * 7))
+// console.log((start.getTime() / (1000 * 3600 * 24)) - (start.getTime()));
+// console.log(start.getTime());
+// const showGraph = () => {
+//   let graph = document.querySelector('.graph')
+//   console.log(graph);  
+// }
